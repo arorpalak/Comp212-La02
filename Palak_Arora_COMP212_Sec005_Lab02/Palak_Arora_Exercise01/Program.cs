b3bcb2190e29e0f4e8ecb2fa46585514605c5ae0
arorpalak <60004971+arorpalak@users.noreply.github.com>
2021-01-05 21:49:28 -0500

    Comp212 Programming 3
    Lab02 Assignment

---
+++ Palak_Arora_COMP212_Sec005_Lab02/Palak_Arora_Exercise01/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//***** Palak Arora - 301112908 *****
+
+namespace Palak_Arora_Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)   //main method
+        {
+            // Exercise 1a - Func<> delegate predicate
+            Console.WriteLine("\n\n ***** Exercise 1a *****");
+            Console.Write("Enter first string: ");   //reading input string
+            string str1 = Console.ReadLine();
+
+            Console.Write("Enter second string: ");  //reading input string
+            string str2 = Console.ReadLine();
+
+            Console.Write("Enter third string: ");   //reading input string
+            string str3 = Console.ReadLine();
+
+            // Func delegate using lambda and the method Minimum
+            Func<string, string, string, string> smallest = (string string1, string string2, string string3) => Minimum(str1, str2, str3); //the forth string is for the output
+            string str = smallest(str1, str2, str3);  //the output string is smallest out of the three strings entered.
+            Console.WriteLine("The smallest string is: " + str);  //displaying the result
+
+            // Exercise 1b - Action<> delegate predicate
+            Console.WriteLine("\n\n ***** Exercise 1b *****");
+
+            Console.Write("Enter first grade: ");             // Read input grades
+            int g1 = (int)Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second grade: ");             // Read input grades
+            int g2 = (int)Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter third grade: ");             // Read input grades
+            int g3 = (int)Convert.ToDouble(Console.ReadLine());
+
+            // Action delegate using lambda and the method AvgGrade
+            Action<int, int, int> average = AvgGrade;
+            average(g1, g2, g3); //calling average action<> delegate predicate
+            Console.ReadLine();
+
+        }//end of main-method
+        public static string Minimum(string string1, string string2, string string3) //method that uses buil-in Func<> delegate predicate to return smallest string
+        {
+            if (string1.Length < string2.Length)   //checks if string1 is smaller than other two strings
+            {
+                if (string1.Length < string3.Length) 
+                { return string1; }
+
+                else
+                { return string3; }
+            }
+            else if (string2.Length < string3.Length)  //checks if strings2 is smaller than string 3
+            {
+                return string2;
+            }
+            else
+            {
+                return string3;
+            }
+        }//end of Minimum Method
+        
+        public static void AvgGrade(int value1, int value2, int value3)  //method that uses built-in Action<> delegate predicate to get average of grades
+        {
+            int result = (value1 + value2 + value3) / 3;  //calculating average
+            Console.WriteLine("The average is: " + result);
+
+        }//end AvgGrade method
+    }
+
+}//end namespace
---
+++ Palak_Arora_COMP212_Sec005_Lab02/Palak_Arora_Exercise02/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//***** Palak Arora - 301112908 *****
+
+namespace Palak_Arora_Exercise02
+{
+    public delegate bool GradesPredicate(double grade);  //a delegate that takes input as a double and returns a bool
+    class Program
+    {
+        static void Main(string[] args) //main method
+        {
+            double[] gradesArray = { 55.5, 11.11, 22.2, 3.33, 44.4, 89.3, 99.9, 64.15, 50, 49.99 };  //array containing grades value.
+
+            Console.Write("\nThe grades of a student: \n");
+            foreach (double item in gradesArray)
+            {
+                Console.Write($" {item} ");
+            }
+            Console.WriteLine();
+            // Filter grades greater than or equal to 50 using an implicit lambda statement
+            GradesFilter(gradesArray, grade => { return grade >= 50; });   
+           
+            Console.ReadLine();
+
+        } // end Main
+
+
+        private static void GradesFilter(double[] gradesArray, GradesPredicate predicate)  // method that displays only the grades that are equal or more than 50
+        {
+            Console.Write("\nThe grades which are greater than or equal to 50 are as follows: \n");
+
+            // Iterate over each element in the array
+            foreach (double item in gradesArray)
+            {
+                // Print elements that satisfies the predicate
+                if (predicate(item))
+                {
+                    Console.Write($" {item} "); // print item followed by a space
+                }
+            }
+            Console.WriteLine();
+        } // end method GradesFilter
+
+    }//end program
+}//end namespace
